7763102b499fcd31f49e8c341cd8c6c6cd626700
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-12 06:21:29 +0100

    12. Register User Api connected

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import customFetch from "../../utils/axios"
+import { toast } from "react-toastify"
 
 const initialState = {
   user: null,
@@ -10,13 +11,14 @@ export const registerUser = createAsyncThunk(
   "user/registerUser",
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post("/auth/testingRegister", user)
-      console.log(resp)
+      const resp = await customFetch.post("/auth/register", user)
+      return resp.data
     } catch (error) {
-      console.log(error.response)
+      return thunkAPI.rejectWithValue(error.response.data.msg)
     }
   }
 )
+
 export const LoginUser = createAsyncThunk(
   "user/loginUser",
   async (user, thunkAPI) => {
@@ -27,6 +29,26 @@ export const LoginUser = createAsyncThunk(
 const userSlice = createSlice({
   name: "user",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      // PENDING STATE
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true
+      })
+      //FULFILED STATE
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload
+        state.isLoading = false
+        state.user = user
+        toast.success(`Hello there ${user.name}`)
+      })
+
+      // REJECTED STATE
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false
+        toast.error(payload)
+      })
+  },
 })
 
 export default userSlice.reducer
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -77,7 +77,7 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type="submit" className="btn btn-block">
+        <button type="submit" className="btn btn-block" disable={isLoading}>
           submit
         </button>
         <p>
