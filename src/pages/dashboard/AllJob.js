ce8a516f9f6b001167aae78c4c20b9e656892b6d
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-21 15:45:15 +0100

    40. AllJobs Page Structure

--- src/pages/dashboard/AllJob.js
+++ src/pages/dashboard/AllJob.js
@@ -1,7 +1,14 @@
 import React from "react"
+import JobsContainer from "../../components/JobsContainer"
+import SearchContainer from "../../components/SearchContainer"
 
 const AllJobs = () => {
-  return <div>AllJob</div>
+  return (
+    <>
+      <JobsContainer />
+      <SearchContainer />
+    </>
+  )
 }
 
 export default AllJobs
