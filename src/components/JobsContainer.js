fc7d0e03eaa1ee7e552e30683ea614250fe1fb76
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-11-21 15:53:32 +0100

    41b. Loading func done

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -2,17 +2,14 @@ import { useEffect } from "react"
 import Job from "./Job"
 import Wrapper from "../assets/wrappers/JobsContainer"
 import { useSelector, useDispatch } from "react-redux"
+import Loading from "./Loading"
 
 const JobsContainer = () => {
   const { jobs, isLoading } = useSelector((store) => store.allJobs)
   const dispatch = useDispatch()
 
   if (isLoading) {
-    return (
-      <Wrapper>
-        <h2>Loading...</h2>
-      </Wrapper>
-    )
+    return <Loading center />
   }
 
   if (jobs.length === 0) {
